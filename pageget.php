05cda7eee84765bf6ede84d10a64b58fe63d09d7
BlueMugi <maya@writing.office.ne.jp>
2016-04-07 07:04:21 +0900

    改訂
    ロードしたときに最新枝番を自動取得

--- pageget.php
+++ pageget.php
@@ -1,23 +1,19 @@
-        <?php
-        ini_set( 'display_errors', 1 );
-        //DB接続
-        $link = mysql_connect('localhost', 'root', 'nekodama1919');
+<?php
+ $link = mysql_connect('localhost', 'root', 'nekodama1919');
                 mysql_query('SET NAMES utf8', $link ); //文字化け防止
                 if (!$link) {die('接続失敗です。'.mysql_error());}   
                 
                 $db_selected = mysql_select_db('tantou', $link);
                 if (!$db_selected){die('データベース選択失敗です。'.mysql_error());}
-  
-                $page = $_POST['quizid'];
-               
-                $result = mysql_query('SELECT * FROM mondai WHERE id ='.$page.';');
+                
+                //クイズIDを最大値の次から自動取得する
+                $result = mysql_query('SELECT MAX(quizid) FROM kaitou');
                 if (!$result) {die('クエリーが失敗しました。'.mysql_error());}
                 //DBとじる
                 $close_flag = mysql_close($link);       
                 
                 $arr = mysql_fetch_assoc($result);
-                
-                $resultarr = json_encode($arr);
-                echo $resultarr;  
+                echo $arr ["MAX(quizid)"];
         
         ?>
+
