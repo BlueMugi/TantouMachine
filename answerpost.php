a36ae7b28e6f0fe9063ca448f4af52958601f087
BlueMugi <BlueMugi@users.noreply.github.com>
2016-04-06 21:53:46 +0900

    Added files via upload

---
+++ answerpost.php
@@ -0,0 +1,23 @@
+<?php
+        //DBに回答結果を記録する
+        date_default_timezone_set('Asia/Tokyo');
+        //DB接続
+        $link = mysql_connect('localhost', 'root', 'password');
+                mysql_query('SET NAMES utf8', $link ); //文字化け防止
+                if (!$link) {die('接続失敗です。'.mysql_error());}   
+                
+                $db_selected = mysql_select_db('tantou', $link);
+                if (!$db_selected){die('データベース選択エラ～'.mysql_error());}
+  
+                $quizid = $_POST['quizid'];
+                $result = $_POST['result'];
+                $text = $_POST['comment'];
+                $kasegi = $_POST['kasegi'];
+                $timestamp=date('YmdHis');  
+                
+                $result=mysql_query('INSERT INTO tantou.kaitou (quizid, result, comment, kasegi, ansday) VALUES ('.$quizid.', '.$result.', "'.$text.'",'.$kasegi.','.$timestamp.');');
+                
+                if (!$result) {die('クエリエラ～'.mysql_error());}
+                //DBとじる
+                $close_flag = mysql_close($link);        
+?>
