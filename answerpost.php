14504420469817a26be6c634fa037f7bdaa548e8
BlueMugi <maya@writing.office.ne.jp>
2016-04-08 11:58:31 +0900

    BugFix
    回答記録テーブルに枝番号も投げる、クイズIDのインクリメントを回答後に、初期に回答データがない場合のエラー回避

--- answerpost.php
+++ answerpost.php
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 <?php
         //DBに回答結果を記録する
         date_default_timezone_set('Asia/Tokyo');
         //DB接続
-        $link = mysql_connect('localhost', 'root', 'password');
+        $link = mysql_connect('localhost', 'root', 'nekodama1919');
                 mysql_query('SET NAMES utf8', $link ); //文字化け防止
                 if (!$link) {die('接続失敗です。'.mysql_error());}   
                 
@@ -11,39 +10,15 @@
                 if (!$db_selected){die('データベース選択エラ～'.mysql_error());}
   
                 $quizid = $_POST['quizid'];
+                $branch = $_POST['branch'];
                 $result = $_POST['result'];
                 $text = $_POST['comment'];
                 $kasegi = $_POST['kasegi'];
                 $timestamp=date('YmdHis');  
                 
-                $result=mysql_query('INSERT INTO tantou.kaitou (quizid, result, comment, kasegi, ansday) VALUES ('.$quizid.', '.$result.', "'.$text.'",'.$kasegi.','.$timestamp.');');
-                
-                if (!$result) {die('クエリエラ～'.mysql_error());}
-
-                //DBとじる
-                $close_flag = mysql_close($link);        
-=======
-<?php
-        //DBに回答結果を記録する
-        date_default_timezone_set('Asia/Tokyo');
-        //DB接続
-        $link = mysql_connect('localhost', 'root', 'password');
-                mysql_query('SET NAMES utf8', $link ); //文字化け防止
-                if (!$link) {die('接続失敗です。'.mysql_error());}   
-                
-                $db_selected = mysql_select_db('tantou', $link);
-                if (!$db_selected){die('データベース選択エラ～'.mysql_error());}
-  
-                $quizid = $_POST['quizid'];
-                $result = $_POST['result'];
-                $text = $_POST['comment'];
-                $kasegi = $_POST['kasegi'];
-                $timestamp=date('YmdHis');  
-                
-                $result=mysql_query('INSERT INTO tantou.kaitou (quizid, result, comment, kasegi, ansday) VALUES ('.$quizid.', '.$result.', "'.$text.'",'.$kasegi.','.$timestamp.');');
+                $result=mysql_query('INSERT INTO tantou.kaitou (quizid, branch, result, comment, kasegi, ansday) VALUES ('.$quizid.', "'.$branch.'", '.$result.', "'.$text.'",'.$kasegi.','.$timestamp.');');
                 
                 if (!$result) {die('クエリエラ～'.mysql_error());}
                 //DBとじる
                 $close_flag = mysql_close($link);        
->>>>>>> origin/master
 ?>
--- pageget.php
+++ pageget.php
@@ -6,14 +6,24 @@
                 $db_selected = mysql_select_db('tantou', $link);
                 if (!$db_selected){die('データベース選択失敗です。'.mysql_error());}
                 
-                //クイズIDを最大値の次から自動取得する
-                $result = mysql_query('SELECT MAX(quizid) FROM kaitou');
-                if (!$result) {die('クエリーが失敗しました。'.mysql_error());}
-                //DBとじる
-                $close_flag = mysql_close($link);       
                 
+                //データ数(初期状態かどうか)
+                $data=mysql_query('SELECT EXISTS(SELECT * FROM kaitou WHERE quizid)');
+                if (!$data) {die('データ来ません'.mysql_error());}
+                
+                
+                $earr = mysql_fetch_array($data);
+                if ($earr[0]==0){
+                    echo "0";
+                }else{
+                //クイズIDを最大値の次から自動取得する
+                $result = mysql_query('SELECT MAX(quizid) FROM kaitou');    
+                if (!$result) {die('クエリーが失敗しました。'.mysql_error());}    
                 $arr = mysql_fetch_assoc($result);
-                echo $arr ["MAX(quizid)"];
-        
+                echo $arr ["MAX(quizid)"]; 
+                }
+                
+                $close_flag = mysql_close($link);  
+
         ?>
 
