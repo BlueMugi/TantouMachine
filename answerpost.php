2dd8064c4b1d3c6acf2294e86c5b74714eaed440
BlueMugi <BlueMugi@users.noreply.github.com>
2016-04-09 07:39:03 +0900

    Update answerpost.php

--- answerpost.php
+++ answerpost.php
@@ -2,7 +2,7 @@
         //DBに回答結果を記録する
         date_default_timezone_set('Asia/Tokyo');
         //DB接続
-        $link = mysql_connect('localhost', 'root', 'nekodama1919');
+        $link = mysql_connect('localhost', 'root', 'password');
                 mysql_query('SET NAMES utf8', $link ); //文字化け防止
                 if (!$link) {die('接続失敗です。'.mysql_error());}   
                 
